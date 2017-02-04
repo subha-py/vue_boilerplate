51d5c68c6a6f54d92eb197acd242673c21c37d58
subha bera <subha.py@gmail.com>
2017-02-05 02:44:01 +0530

    updated with todo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
+    <CompletedToDo></CompletedToDo>
+    <GetToDo></GetToDo>
+    <CurrentToDo></CurrentToDo>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app'
-}
+  import GetToDo from './components/GetToDo'
+  import CurrentToDo from './components/CurrentToDo'
+  import CompletedToDo from './components/CompletedToDo'
+
+  export default {
+    name: 'app',
+    components: {
+      GetToDo,
+      CurrentToDo,
+      CompletedToDo
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: Helvetica, sans-serif;
-}
+  #app {
+    font-family: Helvetica, sans-serif;
+  }
 </style>
