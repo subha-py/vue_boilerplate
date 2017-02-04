85c2b75cc63ef00866e7a4315c17d3eb54488ea7
subha bera <subha.py@gmail.com>
2017-02-05 02:44:46 +0530

    Added components

---
+++ src/components/CurrentToDo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="current-todos" class="container">
+    <h3 v-if="todos.length > 0">Current({{todos.length}})</h3>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="todo in todos">
+        {{todo.body}}
+        <div class="btn-group">
+          <button type="button" @click="edit(todo)" class="btn btn-default btn-sm">
+            <span class="glyphicon glyphicon-edit"></span> Edit
+          </button>
+          <button type="button" @click="complete(todo)" class="btn btn-default btn-sm">
+            <span class="glyphicon glyphicon-ok-circle"></span> Complete
+          </button>
+          <button type="button" @click="remove(todo)" class="btn btn-default btn-sm">
+            <span class="glyphicon glyphicon-remove-circle"></span> Remove
+          </button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default{
+    methods: {
+      edit: function (todo) {
+        this.$store.dispatch('editTodo', todo)
+      },
+      complete: function (todo) {
+        this.$store.dispatch('completeTodo', todo)
+      },
+      remove: function (todo) {
+        this.$store.dispatch('removeTodo', todo)
+      }
+    },
+    computed: {
+      todos: function () {
+        return this.$store.getters.todos
+      }
+    }
+  }
+</script>
+<style>
+  .btn-group{
+    float: right;
+  }
+</style>
---
+++ src/components/GetToDo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="get-todo" class="container">
+    <input class="form-control"
+           :value="newTodo"
+           @change="getTodo"
+           placeholder="I need to...">
+    <button class="btn btn-primary" @click="addTodo">Add Todo</button>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+      getTodo: function (e) {
+        this.$store.dispatch('getTodo', e.target.value)
+      },
+      addTodo: function () {
+        this.$store.dispatch('addTodo')
+        this.$store.dispatch('clearTodo')
+      }
+    },
+    computed: {
+      newTodo: function () {
+        return this.$store.getters.newTodo
+      }
+    }
+  }
+</script>
